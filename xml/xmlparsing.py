f760f41cd74d9a2fb23b8f8debee7dc4a74df9de
Michael Neidhardt <meidhardt@gmail.com>
2022-06-23 09:54:06 +0200

    Updated xmlparsing, e.g. added DFS method to get all paths from XML including text.

--- xml/xmlparsing.py
+++ xml/xmlparsing.py
@@ -18,6 +18,14 @@ class XMLParser():
         result = self.dfswp(self.root, [])
         return result
 
+    def getAllPaths_xsd(self):
+        result = self.dfswp_xsd(self.root, [])
+        return result
+
+    def getAllTextContent(self):
+        result = self.dfstext(self.root)
+        return result
+
     # Remove NS string.
     def cleanupTag(self, tag):
         match = self.NSpattern.match(tag)
@@ -44,3 +52,56 @@ class XMLParser():
                 path.pop()
         
         return result
+      
+    # Does depth first traversal, with path, for XSDs.
+    # The method saves the full path of every leaf,
+    # and also adds info on minOccurs, maxOccurs and type.
+    # Cardinalities is emitted by adding the (sub)path to result
+    # if a node has those attributes and also has kids.
+    # Returns a list of all levels and leaves with full path.
+    def dfswp_xsd(self, node, path):
+        result = []
+        if node is None:
+            return result
+        else:
+            path.append(self.cleanupTag(node.tag))
+
+        if len(node) == 0:
+            if node.text is None:
+                nodetext = ''
+            else:
+                nodetext = node.text    # My XSD parser (parseXSD.py) puts the type information in the text.
+            
+            result.append('/'.join(path) + ';;' + nodetext)
+        else:
+            cardinality = ['1','1']
+            if 'minOccurs' in node.attrib:
+                cardinality[0] = node.attrib['minOccurs']
+            if 'maxOccurs' in node.attrib:
+                cardinality[1] = node.attrib['maxOccurs']
+            result.append('/'.join(path) + ';' + '[' + '->'.join(cardinality) + ']')
+            
+            for child in node:
+                result.extend(self.dfswp_xsd(child, path))
+                path.pop()
+        
+        return result
+      
+    def findPath(self, xpath):
+        return self.root.findall(xpath)
+        
+    # Does depth first traversal looking for text content of all nodes.
+    # Only text content is returned, in a list.
+    def dfstext(self, node):
+        result = []
+        
+        if node is None:
+            return result
+        else:
+            if node.text is not None:
+                result.append(node.text)
+
+            for child in node:
+                result.extend(self.dfstext(child))
+            
+            return result

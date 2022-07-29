7dcfac85e56f40204aa9287d8527dae7a04878a7
Michael Neidhardt <Michael Neidhardt@ufst.dk>
2022-07-29 14:38:03 +0200

    Tried storing only the node used in query. Just for testing. Problem not solved yet.

--- neo4j/serialiseGraph.py
+++ neo4j/serialiseGraph.py
@@ -6,32 +6,33 @@ from neo4j import GraphDatabase
 #-----------------------------------------------------------------------
 
 def serialiseGraph(session, rootdeno):
-    result = [rootdeno]
+    result = []
     serialise(session, rootdeno, result)
     return result
 
-def serialise(session, deno, result):
+def serialise(session, deno, result, path=[]):
+    print('Node:', deno, 'Path:', path)
     query = createQuery(deno)
     g = session.read_transaction( lambda tx: tx.run(query,).graph())
+    result.append(deno)
 
-    # print(deno, 'has', len(g.relationships), 'relations.')
     if len(g.relationships) == 0:
         result.append('!')
         return result
     else:
         for rel in g.relationships:
-            if rel['maxcardinality'] != 1:
-                result.append(rel.start_node['DENo'] + '/' + str(rel['maxcardinality']))
-            else:
-                result.append(rel.start_node['DENo'])
-            serialise(session, rel.start_node['DENo'], result)
+            #if rel['maxcardinality'] != 1:
+            #    result.append(rel.start_node['DENo'] + '/' + str(rel['maxcardinality']))
+            #else:
+            #    result.append(rel.start_node['DENo'])
+            serialise(session, rel.start_node['DENo'], result, path + [rel.start_node['name']])
         result.append('!')
 
 def createQuery(deno):
-    # TODO: serialisering skal tage højde for forældre i alle led bagud/opad, ikke kun den umiddelbare forældre.
+    # TODO: serialisering skal tage højde for forældre i alle led bagud/opad, ikke kun den umiddelbare forælder.
     # Ellers går det galt, når den samme node indgår i forskellige sammenhænge.
     # Dvs. query skal opbygges i takt med at man bevæger sig rundt i grafen, så den fanger alle led i 
-    # den aktuelle sti.
+    # den aktuelle sti (som i createGraf.py).
     return "MATCH (n:Node)-[r:CHILD_OF]->(:Node {DENo:'" + deno + "'}) return n,r;"
 
 userid = sys.argv[1]
